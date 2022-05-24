d3ac59b3ba8ac96eafd8b9155ce1a3574eb1c29a
goncalo <73084553+goncalofso@users.noreply.github.com>
2022-05-24 10:35:08 +0100

    Add files via upload

--- src/main.c
+++ src/main.c
@@ -1,16 +1,187 @@
-#include <zephyr.h>
+/*
+*Assignment 4
+*/
+
+#include <zephyr.h> 
 #include <device.h>
 #include <devicetree.h>
 #include <drivers/gpio.h>
 #include <sys/printk.h>
 #include <sys/__assert.h>
 #include <string.h>
-#include <timing/timing.h>
 #include <stdio.h>
-#include <string.h>
-#include "placa.h"
-#include "Statemachine.h"
 
+#include <timing/timing.h>
+#include <drivers/adc.h>
+
+/* Size of stack area used by each thread (can be thread specific, if necessary)*/
+#define STACK_SIZE 1024
+
+/* Thread scheduling priority */
+#define thread_A_prio 1
+
+/* Therad periodicity (in ms)*/
+#define thread_A_period 20
+
+/* Refer to dts file */
+
+#define BOARDBUT5 0X1F
+
+/* Create thread stack space */
+K_THREAD_STACK_DEFINE(thread_A_stack, STACK_SIZE);
+  
+/* Create variables for thread data */
+struct k_thread thread_A_data;
+
+/* Create task IDs */
+k_tid_t thread_A_tid;
+
+/* Thread code prototypes */
+void thread_A_code(void *, void *, void *);
+
+
+const struct device *gpio0_dev;          /* Pointer to GPIO device structure */
+
+/* Int related declarations */
+static struct gpio_callback but1_cb_data; /* Callback structure */
+
+/* Callback function and variables*/
+volatile int ledstat = 0; /* Led status variable. Updated by the callback function */
+
+/*ADC definitions and includes*/
+#include <hal/nrf_saadc.h>
+#define ADC_NID DT_NODELABEL(adc) 
+#define ADC_RESOLUTION 10
+#define ADC_GAIN ADC_GAIN_1_4
+#define ADC_REFERENCE ADC_REF_VDD_1_4
+#define ADC_ACQUISITION_TIME ADC_ACQ_TIME(ADC_ACQ_TIME_MICROSECONDS, 40)
+#define ADC_CHANNEL_ID 1  
+
+/* This is the actual nRF ANx input to use. Note that a channel can be assigned to any ANx. In fact a channel can */
+/*    be assigned to two ANx, when differential reading is set (one ANx for the positive signal and the other one for the negative signal) */  
+/* Note also that the configuration of differnt channels is completely independent (gain, resolution, ref voltage, ...) */
+#define ADC_CHANNEL_INPUT NRF_SAADC_INPUT_AIN1 
+
+#define BUFFER_SIZE 1
+
+/* Other defines */
+#define TIMER_INTERVAL_MSEC 1000 /* Interval between ADC samples */
+
+/* ADC channel configuration */
+
+static const struct adc_channel_cfg my_channel_cfg = {
+	.gain = ADC_GAIN,
+	.reference = ADC_REFERENCE,
+	.acquisition_time = ADC_ACQUISITION_TIME,
+	.channel_id = ADC_CHANNEL_ID,
+	.input_positive = ADC_CHANNEL_INPUT
+};
+
+/* Global vars */
+struct k_timer my_timer;
+const struct device *adc_dev = NULL;
+static uint16_t adc_sample_buffer[BUFFER_SIZE];
+
+/* Takes one sample */
+/* Takes one sample */
+static int adc_sample(void)
+{
+	int ret;
+	const struct adc_sequence sequence = {
+		.channels = BIT(ADC_CHANNEL_ID),
+		.buffer = adc_sample_buffer,
+		.buffer_size = sizeof(adc_sample_buffer),
+		.resolution = ADC_RESOLUTION,
+	};
+
+	if (adc_dev == NULL) {
+            printk("adc_sample(): error, must bind to adc first \n\r");
+            return -1;
+	}
+
+	ret = adc_read(adc_dev, &sequence);
+	if (ret) {
+            printk("adc_read() failed with code %d\n", ret);
+	}	
+
+	return ret;
+}
+
+
+/* Main function */
 void main(void) {
-  return;
-  }
+
+   int ret;
+
+    /* Welcome message */
+    printk("\n\r Welcome.\n\r");
+
+
+    /* Then create the task */
+    thread_A_tid = k_thread_create(&thread_A_data, thread_A_stack,
+        K_THREAD_STACK_SIZEOF(thread_A_stack), thread_A_code,
+        NULL, NULL, NULL, thread_A_prio, 0, K_NO_WAIT);
+
+    return;
+
+} 
+
+/* Thread code implementation */
+void thread_A_code(void *argA , void *argB, void *argC)
+{
+    /* Local vars */
+    int64_t fin_time=0, release_time=0;     /* Timing variables to control task periodicity */    
+    int ret=0;                              /* Generic return value variable */
+    
+    /* Task init code */
+    printk("Thread A init (periodic)\n");
+
+    /* Compute next release instant */
+    release_time = k_uptime_get() + thread_A_period;
+
+
+     int err=0;
+
+     /* ADC setup: bind and initialize */
+    adc_dev = device_get_binding(DT_LABEL(ADC_NID));
+	if (!adc_dev) {
+        printk("ADC device_get_binding() failed\n");
+    } 
+    err = adc_channel_setup(adc_dev, &my_channel_cfg);
+    if (err) {
+        printk("adc_channel_setup() failed with error code %d\n", err);
+    }
+    
+    /* It is recommended to calibrate the SAADC at least once before use, and whenever the ambient temperature has changed by more than 10 °C */
+    NRF_SAADC->TASKS_CALIBRATEOFFSET = 1;
+    /* Thread loop */
+    while(1) {        
+               
+        err=adc_sample();
+        if(err) {
+            printk("adc_sample() failed with error code %d\n\r",err);
+        }
+        else {
+            if(adc_sample_buffer[0] > 1023) {
+                printk("adc reading out of range\n\r");
+            }
+            else {
+                /* ADC is set to use gain of 1/4 and reference VDD/4, so input range is 0...VDD (3 V), with 10 bit resolution */
+                printk("adc reading: raw:%4u / %4u mV: \n\r",adc_sample_buffer[0],(uint16_t)(1000*adc_sample_buffer[0]*((float)3/1023)));
+            }
+        }
+        
+       
+        /* Wait for next release instant */ 
+        fin_time = k_uptime_get();
+        if( fin_time < release_time) {
+            k_msleep(release_time - fin_time);
+            release_time += thread_A_period;
+
+        }
+    }
+
+    /* Stop timing functions */
+    timing_stop();
+}
+
