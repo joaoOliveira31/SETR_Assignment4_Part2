db9bf35fce1307f8697b34b875575401cb6455ac
João Oliveira <joao.p.oliveira@ua.pt>
2022-05-24 09:55:26 +0100

    Inicialização

---
+++ src/main.c
@@ -0,0 +1,16 @@
+#include <zephyr.h>
+#include <device.h>
+#include <devicetree.h>
+#include <drivers/gpio.h>
+#include <sys/printk.h>
+#include <sys/__assert.h>
+#include <string.h>
+#include <timing/timing.h>
+#include <stdio.h>
+#include <string.h>
+#include "placa.h"
+#include "Statemachine.h"
+
+void main(void) {
+  return;
+  }
